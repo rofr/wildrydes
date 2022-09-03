3e42a1b739e1cd84fee40dfa92b218163134c585
Robert Friberg <robert@devrex.se>
2022-09-03 15:01:03 +0200

    update config, new userpool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_P39KXKsvE', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'e9vvjr3dnqj3p2kog0jm98mu6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_oJCLzVZDz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '39rhvgla68j090hkmons6evbot', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
